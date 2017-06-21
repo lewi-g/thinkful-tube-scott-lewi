b7361e676ae090a474f8fb26688aaa6e041f6caf
mscottcam <mscottcam@gmail.com>
2017-06-21 16:29:04 -0500

    finished product

--- tube-state-management.js
+++ tube-state-management.js
@@ -10,30 +10,38 @@ const stateObject = {
 // state manipulation functions
 //const YouTube_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
 
+
+
+
+// function to add to state before displaying it
+function storeData(state, data) {
+	state.items = data.items
+	displayYouTubeData(state)
+	console.log(stateObject);
+}
+
+// 3 render functions
+function displayYouTubeData(state)/* find data from state*/ {
+  var results = state.items.map(function(item, index) {
+    return renderResult(item);
+  });
+  $('.js-search-results').html(results);
+}
+
 let RESULT_HTML_TEMPLATE = (`
   <div>
     <a class="js-result-name" href="">
       <img class="js-thumbnail" src="">
       <h2 class="title"> </h2>
     </a>
-  </div>`)
- 
+		<a class="channel-url" href="">
+			<h4 class="channel-name"></h4>
+		</a>
+  </div>`);
 
-// function to add to state before displaying it 
-
-
-
-function displayYouTubeData(data)/* find data from state*/ {
-  var results = data.items.map(function(item, index) {
-    return renderResult(item);
-  });
-  $('.js-search-results').html(results);
-}
-
-
-
-// 3 render functions
 function renderResult(result) {
+	let channelName = `${result.snippet.channelTitle}`;
+	let channelId = `http://youtube.com/channel/${result.snippet.channelId}`
 	let videoId = `http://youtube.com/watch?v=${result.id.videoId}`;
 	let thumbnail =  `${result.snippet.thumbnails.medium.url}`;
 	let title = `${result.snippet.title}`
@@ -41,6 +49,9 @@ function renderResult(result) {
   template.find(".js-result-name").attr("href", videoId );
   template.find(".js-thumbnail").attr("src", thumbnail);
   template.find('.title').text(title);
+	template.find('.channel-url').attr("href", channelId);
+	template.find('.channel-name').text(channelName);
+
   // template.find(".js-user-name").text(result.owner.login).attr("href", result.owner.html_url);
 
   return template;
@@ -53,7 +64,6 @@ function getDataFromApi(searchTerm, callback, endpointUrl) {
   	part: 'snippet',
   	key: 'AIzaSyAsg49O_ihb6En3k6NV0K-6e__l4XWZaBI',
     q: searchTerm,
-    per_page: 5
   }
   $.getJSON(endpointUrl, query, callback);
 }
